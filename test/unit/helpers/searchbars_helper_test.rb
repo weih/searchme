2c8f08577632843b1486e7f267cdd9340374153c
weih <xiaohaoprog@gmail.com>
2012-02-12 15:26:54 +0800

    remove /test

--- test/unit/helpers/searchbars_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SearchbarsHelperTest < ActionView::TestCase
-end
--- test/unit/searchbar_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SearchbarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
